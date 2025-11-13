77f89515e2d80f13ae1135915b09d36ba006ab72
AlexRuiz7687 <alexisruiz.daw@gmail.com>
2025-11-13 12:31:59 +0000

    fix(usuario/movimiento): Eliminados los name en las columnas de la entidad movimiento. Eliminada la referencia a la entidad inventario (aún no está creada). Se elimida name:'activo' en usuario.

--- backend/smart-economat-backend/src/modules/movimiento/movimiento.entity/movimiento.entity.ts
+++ backend/smart-economat-backend/src/modules/movimiento/movimiento.entity/movimiento.entity.ts
@@ -4,37 +4,33 @@ import {
   ManyToOne,
   PrimaryGeneratedColumn,
   Check,
-  CreateDateColumn,
   JoinColumn,
 } from 'typeorm';
 import { TipoMovimiento } from '../enums/movimiento.enums';
 import { Usuario } from '../../usuario/usuario.entity/usuario.entity';
-import { Inventario } from '../../inventario/inventario.entity/inventario.entity';
 
 @Entity({ name: 'movimiento' })
 @Check(`"cantidad" > 0`)
-export class Movimiento {
+export class MovimientoEntity {
   @PrimaryGeneratedColumn({ name: 'id_movimiento', type: 'int' })
-  id_movimiento!: number;
+  id!: number;
 
   @Column({
     type: 'enum',
     enum: TipoMovimiento,
-    name: 'tipo',
     enumName: 'tipo_movimiento',
     nullable: false,
   })
   tipo!: TipoMovimiento;
 
-  @Column({ type: 'int', name: 'cantidad', nullable: false })
+  @Column({ type: 'int', nullable: false })
   cantidad!: number;
 
-  @Column({ type: 'text', name: 'descripcion', nullable: true })
+  @Column({ type: 'text', nullable: true })
   descripcion?: string | null;
 
   @Column({
     type: 'timestamp',
-    name: 'fecha',
     default: () => 'CURRENT_TIMESTAMP',
   })
   fecha!: Date;
@@ -46,10 +42,6 @@ export class Movimiento {
   @JoinColumn({ name: 'id_usuario', referencedColumnName: 'id_usuario' })
   usuario!: Usuario;
 
-  @ManyToOne(() => Inventario, (inv) => inv.movimientos, {
-    nullable: false,
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn({ name: 'id_inventario', referencedColumnName: 'id_inventario' })
-  inventario!: Inventario;
+  @Column({ type: 'uuid', nullable: false })
+  inventario!: string;
 }
