f55b4c8e1f40f75f521e472bfa7a2beea9343c74
Maurizio Giovanni <mauriziohernandezcamisa17@gmail.com>
2026-02-10 11:40:06 +0000

    feat: Se crean los DTOs, el controller y el service de pedido y se modifica la entidad pedidos

---
+++ backend/smart-economat-backend/src/modules/pedidos/controller/pedidos.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { CancelPedidoDto } from '../dto/cancelPedido.dto';
+import { UpdatePedidoDto } from '../dto/updatePedido.dto';
+import { PedidoService } from '../service/pedido.service';
+
+@Controller('pedidos')
+export class PedidosController {
+  constructor(private readonly pedidoService: PedidoService) {}
+
+  @Patch(':id/fecha-entrega')
+  updateFechaEntrega(@Param('id') id: string, @Body() dto: UpdatePedidoDto) {
+    return this.pedidoService.updateFechaEntrega(id, dto);
+  }
+
+  @Patch(':id/cancelar')
+  cancelarPedido(@Param('id') id: string, @Body() dto: CancelPedidoDto) {
+    return this.pedidoService.cancelarPedido(id, dto);
+  }
+}
--- backend/smart-economat-backend/src/modules/pedidos/pedido.entity/pedido.entity.ts
+++ backend/smart-economat-backend/src/modules/pedidos/pedido.entity/pedido.entity.ts
@@ -51,6 +51,9 @@ export class Pedido {
 
   @OneToMany(() => RecepcionPedido, (rp) => rp.pedido)
   recepcionesPedido!: RecepcionPedido[];
+
+  @Column({ type: 'text', nullable: true, name: 'motivo_cancelacion' })
+  motivoCancelacion?: string;
 }
 
 export { EstadoPedido };
