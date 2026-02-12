5b0dce44570fc4abb35f056120e61e44d0ef1db3
Darel M. Caballero <darelmartinezcaballero@gmail.com>
2026-02-12 21:34:44 +0000

    update: Cambio de nombre en entidad Producto

--- backend/smart-economat-backend/src/seeders/producto.seeder.ts
+++ backend/smart-economat-backend/src/seeders/producto.seeder.ts
@@ -1,4 +1,4 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DeepPartial } from 'typeorm';
 import { Producto } from '../modules/producto/producto.entity/producto.entity';
 import { ProductoProveedor } from '../modules/producto/producto-proveedor.entity/producto-proveedor.entity';
 import { ProductoAlergeno } from '../modules/producto/producto-alergeno.entity/producto-alergeno.entity';
@@ -34,8 +34,8 @@ export const runSeeder = async (dataSource: DataSource) => {
       tipo: faker.helpers.arrayElement(Object.values(TipoProducto)),
       pathImg: faker.image.url({ width: 640, height: 480 }),
       cantidad: faker.number.float({ min: 1, max: 500 }),
-      codigo: faker.string.alphanumeric(10).toUpperCase(),
-    });
+      codigoDeBarra: faker.string.alphanumeric(10).toUpperCase(),
+    } as DeepPartial<Producto>);
     productos.push(producto);
   }
   const productosGuardados = await productoRepo.save(productos);
@@ -59,7 +59,7 @@ export const runSeeder = async (dataSource: DataSource) => {
         precioUnitario: parseFloat(faker.commerce.price({ min: 5, max: 200 })),
         marca: producto.marca,
         codigoBarras: faker.string.numeric({ length: 13 }),
-      });
+      } as DeepPartial<ProductoProveedor>);
       productoProveedores.push(pp);
     }
   }
@@ -79,7 +79,7 @@ export const runSeeder = async (dataSource: DataSource) => {
         productoAlergenoRepo.create({
           producto,
           alergeno,
-        })
+        } as DeepPartial<ProductoAlergeno>)
       );
     }
   }
