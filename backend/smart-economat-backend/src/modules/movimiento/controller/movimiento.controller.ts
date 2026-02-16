caaf0d08aec3ba34a96e764dccd56792d137b93d
Darel M. Caballero <darelmartinezcaballero@gmail.com>
2026-02-16 09:18:26 +0000

    feat: Estandarización de respuestas con interceptors y exceptionfilters

--- backend/smart-economat-backend/src/modules/movimiento/controller/movimiento.controller.ts
+++ backend/smart-economat-backend/src/modules/movimiento/controller/movimiento.controller.ts
@@ -6,14 +6,12 @@ import {
   Param,
   Patch,
   Delete,
-  BadRequestException,
   Query,
 } from '@nestjs/common';
 import { MovimientoService } from '../service/movimiento.service';
 import { CreateMovimientoDto } from '../dto/create-movimiento.dto';
 import { UpdateMovimientoDto } from '../dto/update-movimiento.dto';
 import { MovimientoHistoryDto } from '../dto/movimiento-history.dto';
-import { I18nHelper } from '../../../common/helpers/i18n.helper';
 
 @Controller('movimientos')
 export class MovimientoController {
@@ -21,9 +19,6 @@ export class MovimientoController {
 
   @Post()
   create(@Body() dto: CreateMovimientoDto) {
-    if (!dto) {
-      throw new BadRequestException(I18nHelper.getError('EMPTY_REQUEST'));
-    }
     return this.movimientoService.create(dto);
   }
 
