1676607ebc514d8e84ea1f4027cf8dfdb2ae230e
Darel <darelmartinezcaballero@gmail.com>
2026-03-14 23:19:20 +0000

    fix: rastros de merge

--- backend/smart-economat-backend/src/app.module.ts
+++ backend/smart-economat-backend/src/app.module.ts
@@ -18,10 +18,7 @@ import { InventarioModule } from './modules/inventario/inventario.module';
 import { AlbaranModule } from './modules/albaran/albaran.module';
 import { UbicacionModule } from './modules/ubicacion/ubicacion.module';
 import { IncidenciaModule } from './modules/incidencia/incidencia.module';
-<<<<<<< HEAD
 import { ArchivoModule } from './modules/archivo/archivo.module';
-=======
->>>>>>> 9a12607 (feat: Sistema educacional de gestión de usuarios v1)
 import { ProfesorModule } from './modules/profesor/profesor.module';
 import { AlumnoModule } from './modules/alumno/alumno.module';
 import { AdminModule } from './modules/admin/admin.module';
@@ -54,10 +51,7 @@ import { RecepcionDraftModule } from './modules/recepcion-draft/recepcion-draft.
     AlbaranModule,
     UbicacionModule,
     IncidenciaModule,
-<<<<<<< HEAD
     ArchivoModule,
-=======
->>>>>>> 9a12607 (feat: Sistema educacional de gestión de usuarios v1)
     ProfesorModule,
     AlumnoModule,
     AdminModule,
