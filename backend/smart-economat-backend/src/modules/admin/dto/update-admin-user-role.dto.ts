62cc564748c930c9446b97756998219515f8d501
AlexRuiz7687 <alexisruiz.daw@gmail.com>
2026-03-19 14:21:25 +0000

    fix: errores de login/registro y visualizacion de estado activo/inactivo bucle infinito

--- backend/smart-economat-backend/src/modules/admin/dto/update-admin-user-role.dto.ts
+++ backend/smart-economat-backend/src/modules/admin/dto/update-admin-user-role.dto.ts
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 import { IsArray, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
+=======
+import { IsNotEmpty, IsUUID } from 'class-validator';
+>>>>>>> eb5618a (fix: errores de login/registro y visualizacion de estado activo/inactivo bucle infinito)
 
 export class UpdateAdminUserRoleDto {
   @IsUUID()
   @IsNotEmpty()
   roleId!: string;
+<<<<<<< HEAD
 
   @IsArray()
   @IsOptional()
@@ -14,4 +19,6 @@ export class UpdateAdminUserRoleDto {
   @IsOptional()
   @IsUUID('all', { each: true })
   permisosExcluidosIds?: string[];
+=======
+>>>>>>> eb5618a (fix: errores de login/registro y visualizacion de estado activo/inactivo bucle infinito)
 }
