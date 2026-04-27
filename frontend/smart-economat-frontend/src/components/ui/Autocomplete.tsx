4d6ea18aaacd24399c8bb5c2e08fb6af108bffda
Darel M. Caballero <darelmartinezcaballero@gmail.com>
2026-04-27 15:14:37 +0100

    Fix definitivo de lint y formato para el despliegue

--- frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
+++ frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
@@ -116,9 +116,7 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
         />
       )}
       renderOption={(props, option) => {
-        const { key, ...optionProps } = props as React.HTMLAttributes<HTMLLIElement> & {
-          key: string;
-        };
+        const { key, ...optionProps } = props as any;
         return (
           <Box
             component="li"
