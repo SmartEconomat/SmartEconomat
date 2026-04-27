7c0c21f49597092f7aee50321a25a1410a0252b2
Darel M. Caballero <darelmartinezcaballero@gmail.com>
2026-04-27 15:20:33 +0100

    Eliminar uso de any y corregir tipado en Autocomplete

--- frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
+++ frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
@@ -116,7 +116,9 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
         />
       )}
       renderOption={(props, option) => {
-        const { key, ...optionProps } = props as any;
+        const { key, ...optionProps } = props as React.HTMLAttributes<HTMLLIElement> & {
+          key: string;
+        };
         return (
           <Box
             component="li"
