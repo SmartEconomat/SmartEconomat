ea82b5e86d170ce93132f9286f8f00ca99b6c0c2
Darel M. Caballero <darelmartinezcaballero@gmail.com>
2026-04-27 15:13:08 +0100

    Eliminar globales redundantes y fijar formato de Autocomplete

--- frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
+++ frontend/smart-economat-frontend/src/components/ui/Autocomplete.tsx
@@ -116,7 +116,9 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
         />
       )}
       renderOption={(props, option) => {
-        const { key, ...optionProps } = props as React.HTMLAttributes<HTMLLIElement> & { key: string };
+        const { key, ...optionProps } = props as React.HTMLAttributes<HTMLLIElement> & {
+          key: string;
+        };
         return (
           <Box
             component="li"
